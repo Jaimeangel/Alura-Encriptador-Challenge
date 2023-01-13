0cc730adb6faa98ac0c3b159c4b4ed7a2334c9da
Jaime A <jaimeangel1097@gmail.com>
2023-01-13 15:33:54 -0500

    funcionalidad para visualizar el area de copiado completo

--- index.js
+++ index.js
@@ -4,19 +4,29 @@ const btn_encrypt=document.querySelector('.--encrip')
 const btn_decrypt=document.querySelector('.--descrip')
 const btn_copy=document.querySelector('.--copy')
 
+function no_content_copy_area(){
+    if(copy_area.textContent){
+        document.querySelector('.no_content').classList.add('hide')
+        document.querySelector('.copy_area').classList.add('show')
+    }
+}
 
 function encrypt(){
     const msg=write_area.value;
     const encode_msg=btoa(msg)
     console.log(encode_msg)
+    console.log(typeof(encode_msg))
     copy_area.textContent=`${encode_msg}`
+    no_content_copy_area()
 }
 
 function decrypt(){
     const msg=write_area.value;
     const encode_msg=atob(msg)
     console.log(encode_msg)
+    console.log(typeof(encode_msg))
     copy_area.textContent=`${encode_msg}`
+    no_content_copy_area()
 }
 
 function copy_text(){
